1a24ebf90754914b4d3a78b1cafb5f57775895ba
Chevy Ray Johnston <happytrash@gmail.com>
2022-11-16 23:13:22 -0800

    improve thumbnails and add date option

--- static/js/date-formatter.js
+++ static/js/date-formatter.js
@@ -1,6 +1,7 @@
 //let dates = Array.from(document.geElementsBy("date"));
-let dateElem = document.getElementById("date");
-if (dateElem) {
+//let dateElem = document.getElementById("date");
+let dates = document.getElementsByClassName("date");
+for (const dateElem of dates) {
     let date = new Date(dateElem.textContent);
     const options = { year: "numeric", month: "long", day: "numeric" };
     dateElem.textContent = date.toLocaleDateString("en-US", options);
